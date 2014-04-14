b1d59e43a1667a33888ea8bdb907f79b306af11a
Chris Reeves <chris@thisissoon.com>
2014-04-14 12:56:18 +0100

    Updated documentation

--- docs/api/views.rst
+++ docs/api/views.rst
@@ -4,6 +4,14 @@ Views
 Use these views as a drop in replacement for Flask ``MethodView``. These
 Views provide basic generic functionality for building standard Views.
 
+flask_velox.views.sqla.model
+----------------------------
+
+.. automodule:: flask_velox.views.sqla.model
+    :members:
+    :private-members:
+    :show-inheritance:
+
 flask_velox.views.template
 --------------------------
 
--- flask_velox/views/template.py
+++ flask_velox/views/template.py
@@ -7,20 +7,20 @@ Example
 -------
 
 >>> from flask import Flask
-... from flask.ext.velox.views.template import TemplateView
-... from flask.views import MethodView
+>>> from flask.ext.velox.views.template import TemplateView
+>>> from flask.views import MethodView
 ...
-... app = Flask(__name__)
+>>> app = Flask(__name__)
 ...
-... class HomeView(TemplateView):
+>>> class HomeView(TemplateView):
 ...     template = 'templates/home.html'
 ...     context = {
 ...         'hello': 'word'
 ...     }
 ...
-... app.add_url_rule('/', view_func=HomeView.as_view('home'))
+>>> app.add_url_rule('/', view_func=HomeView.as_view('home'))
 ...
-... app.run()
+>>> app.run()
 
 """
 
