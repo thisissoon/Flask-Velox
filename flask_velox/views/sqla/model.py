828555cb2d78cc0577053e183dc88560650bc9ae
Chris Reeves <chris@thisissoon.com>
2014-04-14 12:56:03 +0100

    Basic List Model View

---
+++ flask_velox/views/sqla/model.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module provides classes for rendering views for SQLAlchemy based models.
+"""
+
+from flask_velox.mixins.context import ContextMixin
+from flask_velox.mixins.sqla.model import ListModelMixin
+from flask_velox.mixins.template import TemplateMixin
+
+
+class ModelListView(ListModelMixin, ContextMixin, TemplateMixin):
+    """
+    Handles rendering templates which list out model objects.
+
+    Example
+    -------
+    >>> from flask import Flask
+    >>> from flask.ext.sqlalchemy import SQLAlchemy
+    >>> from flask.ext.velox.views.sqla.model import ModelListView
+    >>> from yourapp.models import SomeModel
+    ...
+    >>> app = Flask(__name__)
+    >>> db = SQLAlchemy(app)
+    ...
+    >>> class MyListView(ModelListView):
+    ...     template = 'templates/list.html'
+    ...     session = db.session
+    ...     model = SomeModel
+    ...
+    >>> app.add_url_rule('/', view_func=MyListView.as_view('list'))
+    ...
+    >>> app.run()
+
+    """
+
+    pass
